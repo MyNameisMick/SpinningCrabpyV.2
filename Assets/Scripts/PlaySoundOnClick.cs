f2a9df71bbb3ddf499670f315c8869de8585da5a
Lottojoy <132135672+Lottojoy@users.noreply.github.com>
2025-01-26 11:32:56 +0700

    finalMainmenu

---
+++ Assets/Scripts/PlaySoundOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnClick : MonoBehaviour
+{
+    public AudioSource audioSource; // อ้างถึง AudioSource
+    public AudioClip clickSound; // ใส่เสียงที่ต้องการเล่น
+
+    public void PlaySound()
+    {
+        if (audioSource != null && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+    }
+}
